e39df8bcf80b514b94bf24d2b9ac54b1e11bad88
Ravindra Sahu <ravindra.sahu@aurx.co>
2023-03-09 15:09:08 +0530

    change code

--- pattern.java
+++ pattern.java
@@ -1,21 +1,13 @@
-public class RightTrianglePattern   
-{   
+public class RightTrianglePattern {
 public static void main(String args[])   
-{   
-//i for rows and j for columns      
-//row denotes the number of rows you want to print  
-int i, j, row=6;   
-//outer loop for rows  
-for(i=0; i<row; i++)   
-{   
-//inner loop for columns  
-for(j=0; j<=i; j++)   
-{   
-//prints stars   
-System.out.print("* ");   
-}   
-//throws the cursor in a new line after printing each line  
-System.out.println();   
-}   
-}   
+{
+    for(int i=0;i<5;i++)
+    {
+        for (int j=0;j<5;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
 }  
